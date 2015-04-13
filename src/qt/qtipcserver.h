50f18f3c1cae8e9dd4db3263bb1fac20489b6ae9
tcoin666 <tcoin666@ivore.com>
2015-04-13 18:18:16 +0200

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define tcoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "tcoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
